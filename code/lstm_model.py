1139092fa629351afb3385bfa9ffe453aef186b3
Andrew X. Ding <andrewdingcanada8@gmail.com>
2021-12-07 20:35:57 -0500

    create lstm model

---
+++ code/lstm_model.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Embedding, LSTM
+from tensorflow.keras.initializers import GlorotNormal, RandomNormal
+
+
+class LSTM():
+  def __init__(self, learning_rate):
+    self.optimizer = Adam(learning_rate=learning_rate)
+    self.model = Sequential()
+  def loss(self, labels, logits):
+    entropy = tf.keras.losses.sparse_categorical_crossentropy(
+      y_true=labels,
+      y_pred=logits,
+      from_logits=True
+    )
+    return entropy
+  def create_model(self, embedding_dim, hidden_dim, vocab_sze):
+    self.model.add(Embedding(
+      vocab_sze,
+      embedding_dim,
+      embeddings_initializer=RandomNormal() #verify randomnormal
+    ))
+    # verify: the nb had return_sequences = true, not sure why (also in rnn assignment)
+    self.model.add(LSTM(
+      embedding_dim,
+      hidden_dim,
+      vocab_sze,
+      return_sequences=True,
+      stateful=True,
+      recurrent_initializer=GlorotNormal()
+    ))
+    self.model.add(Dense(vocab_sze))
+    self.model.compile(
+      optimizer=self.optimizer,
+      loss=self.loss
+    )
+    return self.model
+  
